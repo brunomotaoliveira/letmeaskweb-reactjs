b19beee79df9c564e5de307ae901fcf2bce7be34
brunomotadev <brunomotadev@gmail.com>
2022-03-28 18:04:02 -0300

    autenticacao do usuario com google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 
+import { auth, firebase } from '../services/firebase';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -15,6 +17,12 @@ export function Home() {
 
 
     function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+        })
+
         navigate('/rooms/new');
     }
 

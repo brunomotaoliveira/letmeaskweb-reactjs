d66c0f5d91b3c6f37a8652ffe583df63c8fffe98
brunomotadev <brunomotadev@gmail.com>
2022-03-28 17:42:51 -0300

    criado as 2 telas com reactrouterdom6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from "./components/Button";
+
+
+import React from "react";
+import { BrowserRouter,Routes, Route} from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <div>
-    <Button />
-    <Button />
-    <Button />
-    <Button />
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

6f8bb68a79861097532ac64763cc7b6b52cff779
brunomotadev <brunomotadev@gmail.com>
2022-03-31 18:05:35 -0300

    spread operator

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
     return (
-        <button className="button" {...props} />
+        <button className="button" {...props} /> //spread operator
     )
 }
 

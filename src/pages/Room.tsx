82282fe6955c5607a70e354d405f4b11025b7884
brunomotadev <brunomotadev@gmail.com>
2022-04-08 17:53:36 -0300

    useeffect em room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,10 +1,13 @@
 import { type } from '@testing-library/user-event/dist/type';
+import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 import logoImg from '..//assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -13,14 +16,51 @@ type RoomParams = {
 }
 
 export function Room() {
+    const { user } = useAuth();
     const params = useParams<RoomParams>(); //generic parametro que passa para tipagem
+    const [newQuestion, setNewQuestion] = useState('');    
+    
+    const roomId = params.id;
+
+    useEffect(() => {
+        const roomRef = database.ref(`rooms/${roomId}`);
+
+        roomRef.once('value', room => {
+            console.log(room.val());
+        })
+    }, []);
+
+    async function handleSendQuestion(event: FormEvent) {
+        event.preventDefault();
+
+        if(newQuestion.trim() === '') {
+            return;
+        }
+
+        if(!user) {
+            throw new Error('You must  be logged in');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name: user?.name,
+                avatar: user.avatar,
+            },
+            isHighlighted: false,
+            isAnswered: false
+        };
+
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+
+        setNewQuestion('');    }
 
     return (
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <RoomCode code={params.id} />
+                    <RoomCode code={roomId} />
                 </div>
             </header>
 
@@ -30,14 +70,24 @@ export function Room() {
                     <span>4 perguntas</span>
                 </div>
 
-                <form>
+                <form onSubmit={handleSendQuestion}>
                     <textarea 
                         placeholder='O que você quer perguntar?'
+                        onChange={event => setNewQuestion(event.target.value)}
+                        value={newQuestion}
+
                     />
 
                     <div className="form-foooter">
-                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
-                        <Button type='submit'>Enviar pergunta</Button>
+                        { user ? (
+                            <div className='user-info'>
+                                <img src={user.avatar} alt={user.name} />
+                                <span>{user.name}</span>
+                            </div>
+                        ) : (
+                            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+                        )}
+                        <Button type='submit' disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
             </main>

db55c595fea2500a0fb74e4a1836de4dc5114508
brunomotadev <brunomotadev@gmail.com>
2022-04-08 12:22:41 -0300

    criado a estilização para room

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,4 +1,7 @@
 import logoImg from '..//assets/images/logo.svg';
+import { Button } from '../components/Button';
+
+import '../styles/room.scss';
 
 export function Room() {
     return (
@@ -6,8 +9,27 @@ export function Room() {
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
+                    <div>codigo</div>
                 </div>
             </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder='O que você quer perguntar?'
+                    />
+
+                    <div className="form-foooter">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button type='submit'>Enviar pergunta</Button>
+                    </div>
+                </form>
+            </main>
         </div>
     );
 }

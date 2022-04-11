f3146b33567b302405aa801627e9175b51e1059c
brunomotadev <brunomotadev@gmail.com>
2022-04-11 19:29:44 -0300

    funcionalidade de likes finalizada

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -5,7 +7,8 @@ type QuestionProps = {
     author: {
         name: string;
         avatar: string;
-    }
+    };
+    children?: ReactNode;
 
 }
 
@@ -14,6 +17,7 @@ type QuestionProps = {
 export function Question({
     content,
     author,
+    children,
 }: QuestionProps) {
     return(
         <div className="question">
@@ -23,7 +27,9 @@ export function Question({
                     <img src={author.avatar} alt={author.name} />
                     <span>{author.name}</span>
                 </div>
-                <div></div>
+                <div>
+                    {children}
+                </div>
             </footer>
         </div>
     );
